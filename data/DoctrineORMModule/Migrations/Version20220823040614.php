d318dc945ad16a3f4b47c94096717e6729f801bc
Rodrigo Isidro <rodrigo.isidro@mutua.com.br>
2022-08-23 01:11:26 -0300

    Configuration migrate and create table post

---
+++ data/DoctrineORMModule/Migrations/Version20220823040614.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DoctrineORMModule\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20220823040614 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->addSql("
+        create table public.post
+        (
+            id          uuid         not null primary key ,
+            title       varchar(100) not null,
+            description varchar(5000) not null
+        )"); 
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
